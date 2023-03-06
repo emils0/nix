f46c300af62c0b40192f89059f294992109f2228
Emil <7209247-emil-s@users.noreply.gitlab.com>
2023-03-06 10:09:00 +0100

    Added packages for WSL.

--- home.nix
+++ home.nix
@@ -1,17 +1,35 @@
-{config, pkgs, ...}:
-
 {
-home = {
-username = "emil";
-homeDirectory = "/home/emil";
-  stateVersion = "22.11";
+  config,
+  pkgs,
+  ...
+}: {
+  home = {
+    username = "emil";
+    homeDirectory = "/home/emil";
+    stateVersion = "22.11";
   };
 
-programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 
   programs.git.enable = true;
+  programs.fish.enable = true;
+  programs.bash.enable = true;
+  targets.genericLinux.enable = true;
 
   imports = [
     ./common
+    ./wsl/packages.nix
   ];
+
+  programs.exa.enable = true;
+
+  programs.zoxide = {
+    enable = true;
+    options = ["--cmd s"];
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
 }
---
+++ wsl/packages.nix
@@ -0,0 +1,69 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    alejandra
+    git-ignore
+    comma
+    cachix
+    exa
+    ripgrep
+    wget
+    unzip
+    bottom
+    yt-dlp
+    curl
+    ffmpeg
+    git
+    gnupg
+    asciinema
+    fd
+    so
+    dotnet-sdk
+
+    nodejs
+    rnix-lsp
+    sqls
+    rust-analyzer
+    jsonnet
+    clang-tools
+    delve
+    rustfmt
+    black
+    nixfmt
+    pandoc
+
+    cargo
+    gradle
+    gnumake
+    cmake
+    libtool
+    rustc
+    go
+    clippy
+    cargo-edit
+    cargo-watch
+
+    nodePackages.npm
+    nodePackages.typescript
+    nodePackages.typescript-language-server
+    nodePackages.svelte-language-server
+    nodePackages.prettier
+    nodePackages.mermaid-cli
+
+    (
+      let
+        my-python-packages = python-packages:
+          with python-packages; [
+            jedi
+            python-lsp-server
+            pylint
+            flake8
+            rope
+            isort
+            mypy
+          ];
+        python-with-my-packages = python3.withPackages my-python-packages;
+      in
+        python-with-my-packages
+    )
+  ];
+}
